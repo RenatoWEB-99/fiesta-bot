e954166ade1aee9cf5261c870ebf0a7fe36e057b
ReneeIDK <62621126+RenatoWEB-99@users.noreply.github.com>
2023-01-25 23:55:53 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,30 @@ async def send_message(message, user_message, is_private):
         print(e)
 
 def run_discord_bot():
+
     TOKEN = ""
-    client = discord.Client()
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
         print(f"{client.user} is now running!")
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
