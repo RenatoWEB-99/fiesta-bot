5602651fc9a4de58540ee3c3c266f5d18e1e8b72
ReneeIDK <62621126+RenatoWEB-99@users.noreply.github.com>
2023-01-26 00:04:51 -0300

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,14 +1,16 @@
-import math
 import random
 
-def handle_response(message) -> str
+def handle_response(message) -> str:
     p_message = message.lower()
 
-    if p_message == "oi"
+    if p_message == "oi":
         return "opa"
 
-    if p_message == ":roll"
-        return str(random.radiant(1, 6))
+    if p_message == ":roll":
+        return str(random.randint(1, 6))
 
-    if p_message == ":help"
+    if p_message == ":help":
         return "`Você acabou de usar o comando :help. Parece que você precisa de ajuda!\nMeu prefixo padrão é ':'.\n:roll - Rola um número aleatório entre 1 e 6.\n`"
+
+    if p_message == "você está no canal":
+        return "simpson gamer"
